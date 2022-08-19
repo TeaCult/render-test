a0b6a875014a2eaee4612ba7af691609bd6b20d1
Gediz GÜRSU <gedizgursu@gmail.com>
2022-08-19 09:51:35 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,19 +7,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello from Render!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-
           <a
             className="App-link"
             href="/lines.txt"
